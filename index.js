976afed1e246d76b24a06fbc626fe8f7b9316fff
Ryan Headlee <headleeryan@gmail.com>
2022-05-16 11:31:44 -0700

    Add attack function

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ class Sprite {
       height: 50,
     }
     this.color = color;
+    this.isAttacking;
   }
 
   draw() {
@@ -46,6 +47,13 @@ class Sprite {
     } else {
       this.velocity.y += gravity;
     }
+  }
+
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100)
   }
 }
 
@@ -116,8 +124,10 @@ function animate() {
     player.attackBox.position.x + player.attackBox.width >= enemy.position.x   &&
     player.attackBox.position.x <= enemy.position.x + enemy.width &&
     player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-    player.attackBox.position.y <= enemy.position.y + enemy.height 
+    player.attackBox.position.y <= enemy.position.y + enemy.height &&
+    player.isAttacking == true
     ) {
+    player.isAttacking = false;
     console.log("hello");
   }
 }
@@ -136,6 +146,9 @@ window.addEventListener('keydown', (event) => {
       break;
     case 'w':
       player.velocity.y = -18;
+      break;
+    case ' ':
+      player.attack();
       break;
     case 'ArrowRight':
       keys.ArrowRight.pressed = true;

8be3dde01d2619f76e16d332f09a543cf5b9f484
Ryan Headlee <headleeryan@gmail.com>
2022-05-16 10:38:10 -0700

    Add basic jump w/ 'w' key

--- index.js
+++ index.js
@@ -60,6 +60,9 @@ const keys = {
   },
   d: {
     pressed: false,
+  },
+  w: {
+    pressed: false,
   }
 }
 
@@ -92,6 +95,9 @@ window.addEventListener('keydown', (event) => {
       keys.a.pressed = true;
       lastKey = 'a';
       break;
+    case 'w':
+      player.velocity.y = -10;
+      break;
   }
   console.log(event.key);
 });
@@ -103,6 +109,9 @@ window.addEventListener('keyup', (event) => {
       break;
     case 'a':
       keys.a.pressed = false;
+      break;
+    case 'w':
+      keys.w.pressed = false;
       break;
   }
   console.log(event.key);

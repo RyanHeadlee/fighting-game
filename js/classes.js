58875fdb2561cc851740d034f01ba24a9abe5d31
Ryan Headlee <headleeryan@gmail.com>
2022-05-16 16:34:19 -0700

    Offset sprite of player 1

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,5 @@
 class Sprite {
-  constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
+  constructor({ position, imageSrc, scale = 1, framesMax = 1, offset = { x: 0, y: 0 } }) {
     this.position = position;
     this.width = 50;
     this.height = 150;
@@ -10,6 +10,7 @@ class Sprite {
     this.framesCurrent = 0;
     this.framesElapsed = 0;
     this.framesHold = 6;
+    this.offset = offset;
   }
 
   draw() {
@@ -19,8 +20,8 @@ class Sprite {
       0,
       this.image.width / this.framesMax,
       this.image.height,
-      this.position.x,
-      this.position.y,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
       (this.image.width / this.framesMax) * this.scale,
       this.image.height * this.scale
     );
@@ -45,16 +46,17 @@ class Fighter extends Sprite {
     position,
     velocity,
     color = "red",
-    offset,
     imageSrc,
     scale = 1,
     framesMax = 1,
+    offset = { x: 0, y: 0 }
   }) {
     super({
       position,
       imageSrc,
       scale,
       framesMax,
+      offset
     });
 
     this.framesMax = framesMax;
